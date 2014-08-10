2ea874e3cbb0dfea450334ffbdc265c8afa6413f
Martirosian <rmartirosian94@gmail.com>
2014-08-10 14:34:46 +0400

    sof page menu

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-    console.log('I\'m ready!')
+    
+
+    //--------------MENU HOVER-------------------
+
+    var menu_elemtn = $('.container').find('a');
+
+    menu_elemtn.on('mouseenter', menu_hover);
+    menu_elemtn.on('mouseleave', menu_out);
+
+    function menu_hover(){
+    	var _this = $(this);
+    	_this.stop(true, true).animate({backgroundColor:'white', color: '#911B4B'}, 200);
+    } 
+    function menu_out(){
+    	var _this = $(this);
+    	_this.stop(true, true).animate({backgroundColor:'#911B4B', color: 'white'}, 200);
+    } 
 });
