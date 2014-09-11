c2570582856f1cadd1956577f82b51c6e53010af
tsyplyaev <tsyplyaev@gmail.com>
2014-09-11 07:17:28 +0400

    done

--- _site/js/functions.js
+++ _site/js/functions.js
@@ -17,4 +17,37 @@ $(document).ready(function() {
     } 
 
     //--------------MENU HOVER-------------------
+    
+    function search(query) {
+        $(".author_node").each(function() {
+            var el = $(this);
+            var text = "";
+            
+            
+            var ab = el.find(".author_block, .author_block_youth");
+            text = ab.find("a").text() + "@";
+            
+            var rt = el.find(".report_title");
+            text = text + rt.find("span").text();
+            
+            if (query.length === 0 || text.toLowerCase().indexOf(query) > -1)
+                el.show();
+            else
+                el.hide();
+        });
+        
+        $(".section_title, .section_title_youth").each(function() {
+            var el = $(this);
+            var text = el.text().toLowerCase();
+            
+            if (query.length === 0 || text.indexOf(query) > -1)
+                el.show();
+            else
+                el.hide();
+        });
+    }
+    
+    $("#search_text").on('input onpropertychange', function(e) {
+        search(this.value.toLowerCase());
+    });
 });
